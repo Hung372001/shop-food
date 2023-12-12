a9bfd0e7db0a5bb00e2b26590a3cd7309be65e7a
Viet Hung <hunghv@dolphin-techlonpgies.net>
2023-12-12 11:29:50 +0700

    first commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+day la header
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    day la header
+  </div>
+  <slot/>
+  <div>
+    day la footer
+  </div>
+</template>
+
+<style scoped>
+
+</style>
